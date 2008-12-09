861098695f6af7a5d7c9a6fd6d6a64eb32d0e295
rgrp <devnull@localhost>
2008-12-09 12:51:25 +0000

    [miltondata,shksprdata][s]: fix up load commands so works are created as well as material objects.

--- miltondata/load.py
+++ miltondata/load.py
@@ -25,12 +25,18 @@ class LoadTexts(shakespeare.cli.BaseCommand):
         cfgp = SafeConfigParser()
         cfgp.readfp(fileobj)
         for section in cfgp.sections():
-            item = model.Material.byName(section)
+            work = model.Work.by_name(section)
+            if work is None:
+                work = model.Work(name=section)
+            item = model.Material.by_name(section)
             if item is None:
                 item = model.Material(name=section)
             assert item is not None
             for key, val in cfgp.items(section):
+                if key in ['title', 'creator']:
+                    setattr(work, key, val)
                 setattr(item, key, val)
+            item.work = work
             item.src_pkg = pkg
             item.src_locator = '/texts/%s.txt' % section
             model.Session.flush()
