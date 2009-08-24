792bcdb7578ed4243acb84623604d07f1cd56e99
rgrp <devnull@localhost>
2009-08-24 20:21:28 +0000

    [cli,model][s]: follow up to previous commit (r279) to factor out common load_texts stuff from miltondata and shksprdata into shakespeare/model and get it running perfectly with new code.

--- miltondata/cli.py
+++ miltondata/cli.py
@@ -22,23 +22,8 @@ class LoadTexts(shakespeare.cli.BaseCommand):
         import shakespeare.model as model
         pkg = 'miltondata'
         fileobj = pkg_resources.resource_stream(pkg, '/texts/metadata.txt')
-        cfgp = SafeConfigParser()
-        cfgp.readfp(fileobj)
-        for section in cfgp.sections():
-            work = model.Work.by_name(section)
-            if work is None:
-                work = model.Work(name=section)
-            item = model.Material.by_name(section)
-            if item is None:
-                item = model.Material(name=section)
-            assert item is not None
-            for key, val in cfgp.items(section):
-                if key in ['title', 'creator']:
-                    setattr(work, key, val)
-                setattr(item, key, val)
-            item.work = work
-            item.src_pkg = pkg
-            item.src_locator = '/texts/%s.txt' % section
-            model.Session.flush()
+        def locator(section):
+            return u'%s::/texts/%s.txt' % (pkg, section)
 
+        model.load_texts(fileobj, locator)
 
